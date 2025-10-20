33111c174491623bd906dc5b6c59d17daa6f55af
TanLe <tanle20022005@gmail.com>
2025-10-20 11:50:45 +0700

    u i a, ú i à

---
+++ CNPM/Messenger.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace CNPM
+{
+    partial class Messenger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Messenger));
+            this.name_user = new System.Windows.Forms.Label();
+            this.text_input = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanelMessages = new System.Windows.Forms.FlowLayoutPanel();
+            this.pb_reset = new System.Windows.Forms.PictureBox();
+            this.close = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.send_message = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_reset)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.send_message)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name_user
+            // 
+            this.name_user.AutoSize = true;
+            this.name_user.BackColor = System.Drawing.Color.Transparent;
+            this.name_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name_user.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(164)))), ((int)(((byte)(255)))));
+            this.name_user.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.name_user.Location = new System.Drawing.Point(142, 42);
+            this.name_user.Name = "name_user";
+            this.name_user.Size = new System.Drawing.Size(98, 32);
+            this.name_user.TabIndex = 1;
+            this.name_user.Text = "label1";
+            // 
+            // text_input
+            // 
+            this.text_input.Location = new System.Drawing.Point(14, 624);
+            this.text_input.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.text_input.Multiline = true;
+            this.text_input.Name = "text_input";
+            this.text_input.Size = new System.Drawing.Size(432, 50);
+            this.text_input.TabIndex = 3;
+            this.text_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.text_input_KeyDown);
+            // 
+            // flowLayoutPanelMessages
+            // 
+            this.flowLayoutPanelMessages.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(164)))), ((int)(((byte)(255)))));
+            this.flowLayoutPanelMessages.Location = new System.Drawing.Point(0, 111);
+            this.flowLayoutPanelMessages.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanelMessages.Name = "flowLayoutPanelMessages";
+            this.flowLayoutPanelMessages.Size = new System.Drawing.Size(566, 504);
+            this.flowLayoutPanelMessages.TabIndex = 5;
+            this.flowLayoutPanelMessages.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanelMessages_Paint);
+            // 
+            // pb_reset
+            // 
+            this.pb_reset.BackColor = System.Drawing.Color.White;
+            this.pb_reset.Image = ((System.Drawing.Image)(resources.GetObject("pb_reset.Image")));
+            this.pb_reset.Location = new System.Drawing.Point(459, 0);
+            this.pb_reset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb_reset.Name = "pb_reset";
+            this.pb_reset.Size = new System.Drawing.Size(50, 41);
+            this.pb_reset.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_reset.TabIndex = 0;
+            this.pb_reset.TabStop = false;
+            this.pb_reset.Click += new System.EventHandler(this.pb_reset_Click);
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.Color.White;
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.Location = new System.Drawing.Point(514, 0);
+            this.close.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(52, 41);
+            this.close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.close.TabIndex = 6;
+            this.close.TabStop = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            //this.pictureBox3.Image = global::QuanLySinhVien.Properties.Resources.usercut1;
+            this.pictureBox3.Location = new System.Drawing.Point(14, 11);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 88);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // send_message
+            // 
+            this.send_message.Image = ((System.Drawing.Image)(resources.GetObject("send_message.Image")));
+            this.send_message.Location = new System.Drawing.Point(473, 619);
+            this.send_message.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.send_message.Name = "send_message";
+            this.send_message.Size = new System.Drawing.Size(93, 72);
+            this.send_message.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.send_message.TabIndex = 4;
+            this.send_message.TabStop = false;
+            this.send_message.Click += new System.EventHandler(this.send_message_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(566, 115);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Messenger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(561, 688);
+            this.Controls.Add(this.pb_reset);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.flowLayoutPanelMessages);
+            this.Controls.Add(this.send_message);
+            this.Controls.Add(this.text_input);
+            this.Controls.Add(this.name_user);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Messenger";
+            this.Text = "Messenger";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_reset)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.send_message)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label name_user;
+        private System.Windows.Forms.TextBox text_input;
+        private System.Windows.Forms.PictureBox send_message;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelMessages;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox close;
+        private System.Windows.Forms.PictureBox pb_reset;
+    }
+}
---
+++ CNPM/Messenger.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using System.Drawing.Drawing2D;
+
+namespace CNPM
+{
+    public partial class Messenger : Form
+    {
+        String sender_id;
+        String receiver_id;
+        bool flag= true;
+        public Messenger(String sender_id, String receiver_id)
+        {
+            InitializeComponent();
+            this.sender_id=sender_id;
+            this.receiver_id=receiver_id;
+            flowLayoutPanelMessages.AutoScroll = true;
+            LoadMessages();
+            //MessageBox.Show(DateTime.Now.ToString());
+            display();
+        }
+        
+
+        private void LoadMessages()
+        {
+            flowLayoutPanelMessages.Controls.Clear();
+
+            try
+            {
+                using (SqlConnection conn = DatabaseConnection.GetConnection())
+                {
+                    conn.Open();
+                    string query = @"
+                SELECT Người_Gửi, Người_Nhận, Nội_Dung, Thời_Gian 
+                FROM Messagers
+                WHERE (Người_Gửi = @sender_id AND Người_Nhận = @receiver_id)
+                   OR (Người_Gửi = @receiver_id AND Người_Nhận = @sender_id)
+                ORDER BY Thời_Gian";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@sender_id", sender_id);
+                        cmd.Parameters.AddWithValue("@receiver_id", receiver_id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string sender = reader["Người_Gửi"].ToString();
+                                string message = reader["Thời_Gian"].ToString()+reader["Nội_Dung"].ToString();
+                                bool isSender = (sender == sender_id);
+
+                                AddMessageToPanel(EditMessage(message).Item1, EditMessage(message).Item2, isSender);
+                            }
+                        }
+
+                    }
+                }
+                // Cuộn xuống dưới cùng sau khi tải xong tin nhắn
+                flowLayoutPanelMessages.AutoScrollPosition = new Point(0, flowLayoutPanelMessages.VerticalScroll.Maximum);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể tải tin nhắn. Lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AddMessageToPanel(string message,int x, bool isSender)
+        {
+            // Tạo panel chứa tin nhắn
+            Panel messagePanel = new Panel
+            {
+                AutoSize = true,
+                Padding = new Padding(10),
+                Margin = isSender ? new Padding(x, 5, 10, 5) : new Padding(10, 5, 150, 5),
+                BackColor = isSender ? Color.FromArgb(204, 229, 255) : Color.FromArgb(204, 255, 204),
+                MaximumSize = new Size(flowLayoutPanelMessages.Width - 40, 0),
+            };
+
+            // Bo tròn góc panel - chỉ khả thi với Region
+            messagePanel.Paint += (s, e) =>
+            {
+                Panel p = (Panel)s;
+                using (GraphicsPath path = new GraphicsPath())
+                {
+                    int radius = 15;
+                    path.AddArc(0, 0, radius, radius, 180, 90);
+                    path.AddArc(p.Width - radius, 0, radius, radius, 270, 90);
+                    path.AddArc(p.Width - radius, p.Height - radius, radius, radius, 0, 90);
+                    path.AddArc(0, p.Height - radius, radius, radius, 90, 90);
+                    path.CloseAllFigures();
+                    p.Region = new Region(path);
+                }
+            };
+
+            // Tạo label chứa nội dung tin nhắn
+            Label messageLabel = new Label
+            {
+                Text = message,
+                AutoSize = true,
+                MaximumSize = new Size(300, 0), // Giới hạn chiều rộng tin nhắn
+                //Font = new Font("Segoe UI", 10),
+                Font = new Font("Consolas", 10),
+                ForeColor = Color.Black,
+            };
+
+            messagePanel.Controls.Add(messageLabel);
+
+            // Căn bên phải cho tin nhắn người gửi
+            messagePanel.Anchor = isSender ? AnchorStyles.Right : AnchorStyles.Left;
+
+            // Thêm vào flow panel
+            flowLayoutPanelMessages.Controls.Add(messagePanel);
+            flowLayoutPanelMessages.ScrollControlIntoView(messagePanel);
+        }
+
+        private (string, int) EditMessage(string input)
+        {
+            string firstPart = input.Substring(0, 14).Trim() + input[18] + input[19]; 
+
+            string secondPart = input.Substring(20).Trim();
+            int count = 0;
+            int x = 200;
+            if (secondPart.Length > 16)
+            {
+                int y = secondPart.Length;
+                if (y > 23)
+                {
+                    y = 23;
+                }
+                x -= (int)((y-15) * 5.9);
+            }    
+            for (int i = 0; i < secondPart.Length; i++)
+            {
+                count += 1;
+                if (count >= 20)
+                {
+                    if (secondPart[i].ToString() == " " || count == 23)
+                    {
+                        secondPart = secondPart.Substring(0, i) + '\n' + secondPart.Substring(i + 1);
+                        count = 0;
+                    }
+                }
+            }
+            return ($"{firstPart}\n{secondPart}", x);
+        }
+
+
+
+        private void send_message_Click(object sender, EventArgs e)
+        {
+            real_send_message();
+        }
+
+        private void real_send_message()
+        {
+            string messageText = text_input.Text.Trim();
+
+            if (string.IsNullOrEmpty(messageText))
+            {
+                MessageBox.Show("Vui lòng nhập nội dung tin nhắn.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = @"
+                INSERT INTO Messagers (Người_Gửi, Người_Nhận, Nội_Dung, Thời_Gian)
+                VALUES (@SenderId, @ReceiverId, @MessageText, @SentAt)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@SenderId", sender_id);
+                        cmd.Parameters.AddWithValue("@ReceiverId", receiver_id);
+                        cmd.Parameters.AddWithValue("@MessageText", messageText);
+                        cmd.Parameters.AddWithValue("@SentAt", DateTime.Now);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                text_input.Clear();
+                LoadMessages();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể gửi tin nhắn. Lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void display()
+        {
+            bool flag = receiver_id.Trim().StartsWith("SV");
+            string query = "SELECT Họ_và_Tên, Hình_Ảnh FROM Thông_Tin_Sinh_Viên WHERE Mã_Sinh_Viên = @id";
+            if (!flag)
+            {
+                query = "SELECT Họ_và_Tên, Hình_Ảnh FROM Thông_Tin_Giảng_Viên WHERE Mã_Giảng_Viên = @id";
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                  
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", receiver_id);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Gán tên
+                                name_user.Text = reader["Họ_và_Tên"].ToString();
+
+                                // Gán ảnh
+                                if (reader["Hình_Ảnh"] != DBNull.Value)
+                                {
+                                    byte[] imgBytes = (byte[])reader["Hình_Ảnh"];
+                                    using (MemoryStream ms = new MemoryStream(imgBytes))
+                                    {
+                                        pictureBox3.Image = Image.FromStream(ms);
+                                    }
+                                }
+                                else
+                                {
+                                    //pictureBox3.Image = Properties.Resources.usercut; 
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải thông tin sinh viên:\n" + ex.Message);
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pb_reset_Click(object sender, EventArgs e)
+        {
+            LoadMessages();
+        }
+
+        private void flowLayoutPanelMessages_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void text_input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter )
+            {
+                e.SuppressKeyPress = true;
+                real_send_message();      
+            }
+        }
+    }
+}
