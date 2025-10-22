ad8c038484cbeb669380cf6c08ab05d1053491cd
NDKhoa30102005 <ndkhoa30102005@gmail.com>
2025-10-22 23:30:41 +0700

    anh do pc

--- CNPM/DatabaseConnection.cs
+++ CNPM/DatabaseConnection.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 
 namespace CNPM
 {
     public class DatabaseConnection
     {
-        private static string connectionString =
+        private static string connectionString = ConfigurationManager.ConnectionStrings["BanVeTauConnection"].ConnectionString;
+
         //"Data Source=.;Initial Catalog = BanVeTau; Integrated Security = True;";
-        "Data Source=DESKTOP-G8H86K3\\SQLEXPRESS;Initial Catalog = BanVeTau; Integrated Security = True;";
+        //"Data Source=DESKTOP-G8H86K3\\SQLEXPRESS;Initial Catalog = BanVeTau; Integrated Security = True;";
         //"Data Source=LAPTOP-MKNGM2HG;Initial Catalog = BanVeTau; Integrated Security = True;";
         public static SqlConnection GetConnection()
         {
